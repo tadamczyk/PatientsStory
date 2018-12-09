0bb4a90643033e74ce9b7ed189ba8846ead6d14f
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2018-12-09 13:35:36 +0100

    Added needed models - Patient and Visit

---
+++ PatientsStory/PatientsStory/Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace PatientsStory.Models
+{
+    [Table("PATIENT")]
+    public class Patient
+    {
+        [PrimaryKey] [AutoIncrement] public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string PESEL { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<Visit> Visits { get; set; }
+    }
+}
---
+++ PatientsStory/PatientsStory/Models/Visit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace PatientsStory.Models
+{
+    [Table("VISIT")]
+    public class Visit
+    {
+        [PrimaryKey] [AutoIncrement] public int Id { get; set; }
+
+        [ForeignKey(typeof(Patient))] public int PatientId { get; set; }
+
+        public DateTime DateOfVisit { get; set; }
+
+        public string Diagnose { get; set; }
+
+        public string Indications { get; set; }
+
+        public decimal Price { get; set; }
+
+        [ManyToOne(CascadeOperations = CascadeOperation.CascadeRead)]
+        public Patient Patient { get; set; }
+    }
+}
