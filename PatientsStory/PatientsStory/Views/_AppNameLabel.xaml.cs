dc5991f5aa1dcd4aaa422b04388e0f8809ae96ad
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2019-01-01 18:24:21 +0100

    Moved AppName label to shared content view and made some refactor

---
+++ PatientsStory/PatientsStory/Views/_AppNameLabel.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PatientsStory.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class _AppNameLabel : ContentView
+    {
+        public _AppNameLabel()
+        {
+            InitializeComponent();
+        }
+    }
+}
