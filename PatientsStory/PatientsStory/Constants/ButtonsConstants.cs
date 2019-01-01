ddabbb42bd8f4962050fdcc909cc571ad01a51f3
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2019-01-01 14:29:34 +0100

    Added ButtonsConstants class and moved some items from LabelsConstants class

---
+++ PatientsStory/PatientsStory/Constants/ButtonsConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace PatientsStory.Constants
+{
+    public class ButtonsConstants
+    {
+        #region Button Names
+
+        public const string ADD_PATIENT = "Dodaj pacjenta";
+        public const string DELETE_PATIENT = "Usuń pacjenta";
+        public const string EDIT_PATIENT = "Edytuj pacjenta";
+        public const string SAVE_PATIENT = "Zapisz pacjenta";
+
+        public const string ADD_VISIT = "Dodaj wizytę";
+        public const string SHOW_HISTORY = "Wyświetl historię";
+        public const string SAVE_VISIT = "Zapisz wizytę";
+
+        #endregion
+    }
+}
--- PatientsStory/PatientsStory/Constants/LabelsConstants.cs
+++ PatientsStory/PatientsStory/Constants/LabelsConstants.cs
@@ -8,19 +8,6 @@
 
         #endregion
 
-        #region Button Names
-
-        public const string ADD_PATIENT = "Dodaj pacjenta";
-        public const string DELETE_PATIENT = "Usuń pacjenta";
-        public const string EDIT_PATIENT = "Edytuj pacjenta";
-        public const string SAVE_PATIENT = "Zapisz pacjenta";
-
-        public const string ADD_VISIT = "Dodaj wizytę";
-        public const string SHOW_HISTORY = "Wyświetl historię";
-        public const string SAVE_VISIT = "Zapisz wizytę";
-
-        #endregion
-
         #region Fields Names
 
         public const string PATIENT_FIRSTNAME = "Imię";
